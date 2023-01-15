85c04acc293e9b89a90d54870575f9e3b86a9913
Keykhosrow <atrvasa@gmail.com>
2023-01-15 14:06:55 +0330

    Add defaultPatterns class

---
+++ src/main/java/com/atrvasa/infra/DefaultPatterns.java
@@ -0,0 +1,24 @@
+package com.atrvasa.infra;
+
+public class DefaultPatterns {
+    public static String DATE_PATTERN = "yyyy/MM/dd HH:mm:ss";
+
+    public static String SIMPLE_DATE_PATTERN = "yyyy/MM/dd";
+
+    public static String FETCH_ROWS_DELEGATE = "Get%sRows";
+
+    public static String PREPARE_MODEL_DELEGATE = "Prepare%s";
+
+    public static String DELETE_DELEGATE = "Delete%s";
+
+    public static String PERSIST_DELEGATE = "Persist%s";
+
+    public static String REJECT_DELEGATE = "Reject%s";
+
+    public static String UPDATE_DELEGATE = "Update%s";
+
+    public static String CREATE_DELEGATE = "Create%s";
+
+    public static String GET_TABLE_ROWS_DELEGATE = "Get%sRows";
+
+}
