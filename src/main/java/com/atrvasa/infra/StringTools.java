f6b30fa5edc95410b29413e777b03bc5a341f973
Keykhosrow <atrvasa@gmail.com>
2023-01-15 14:04:11 +0330

    Add Desctiption for isNullOrEmpty function

--- src/main/java/com/atrvasa/infra/StringTools.java
+++ src/main/java/com/atrvasa/infra/StringTools.java
@@ -1,6 +1,12 @@
 package com.atrvasa.infra;
 
 public abstract class StringTools {
+    /**
+     * This function returns true if the function's parameter is Null or empty.
+     *
+     * @param str is a string variable
+     * @return boolean
+     */
     public static boolean isNullOrEmpty(String str) {
         if (str == null)
             return true;
